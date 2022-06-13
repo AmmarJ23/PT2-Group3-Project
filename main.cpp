b0746a964917b1120d885df1aa37c4897229e64e
AmmarJ23 <ammar.jmldn@outlook.com>
2022-06-13 16:25:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,37 @@
 #include <iostream>
+using namespace std;
+
+class loginInfo{
+    protected:
+        string userName, password;
+    public:
+
+        loginInfo(string n, string p)
+        {
+            userName = n;
+            password = p;
+        }
+
+        string getUsername(){
+            return userName;
+        }
+
+        string getPassword(){
+            return password;
+        }
+};
+
+
+class User: public loginInfo{ 
+    
+
+
+};
+
+
+int main(){
+
+
+
+    return 0;
+}
