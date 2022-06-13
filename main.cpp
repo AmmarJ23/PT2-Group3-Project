df64b03edeb3ac34af3ae876b404fe5111309e1a
AmmarJ23 <ammar.jmldn@outlook.com>
2022-06-13 16:34:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,15 +23,52 @@ class loginInfo{
 
 
 class User: public loginInfo{ 
-    
+    private:
+        string name, address, vaccine;
+        int phoneNumber;
+    public:
+        User(string Un, string p, string n, string ad, string vc, int pN)
+        {
+            userName = Un;
+            password = p;
+            name = n;
+            address = ad;
+            vaccine = vc;
+            phoneNumber = pN;
+        }
+
 
 
 };
 
+bool loginValidation(User user){
+    bool valid;
+    string userName, password;
+
+    cout << "Enter username: ";
+    cin >> userName;
+    cout << "Enter password: ";
+    cin >> password;
+
+    if (userName.compare(user.getUsername()) == true){
+
+        userName.compare(user.getPassword);
+
+
+    } else
+
+
+    
+
+}
+
 
 int main(){
 
+    
 
 
+
+    system("PAUSE");
     return 0;
 }
