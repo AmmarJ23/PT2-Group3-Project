0fd6844f60e891bed32873fbdd85e4b9d8b330ad
AmmarJ23 <ammar.jmldn@outlook.com>
2022-06-15 23:05:50 +0800

    Made some changes

--- main.cpp
+++ main.cpp
@@ -160,10 +160,96 @@ class hotspotInfo{
         string getHotspotArea(){return hotspotArea;}
 };
 
+//--------------------------------------------guideInfo class------------------------------------------//
+class guideInfo()
+{   
+    
+    private:
+    
+    public:
+    int choice;
+
+    while (true)
+    {
+        menuPrint(3); //print guideInfo screen
+
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                {
+                    cout<<"CARE PROVIDED IN UTM FOR COV-19 PATIENTS"<<endl;
+                    cout<<"----------------------------------------------"<<endl;
+                    cout<<"COMPLIMENTARY MEALS(3 TIMES PER DAY)"<<endl;
+                    cout<<"COMPLIMENTARY SELF TEST KIT(AFTER QUARANTINE IS COMPLETED)"<<endl;
+                    cout<<"COMPLIMENTARY MEDICINE(FOR SYMPTOMS)"<<endl;
+                    
+                    cout<<"-------------------------------------------"<<endl;
+                    cout<<"FREE SWAB TEST AND QUARANTINE AREA PROVIDED"<<endl;
+                    cout<<"AT KOLEJ TUN RAZAK(KTR)"<<endl;
+                    cout<<"-------------------------------------------"<<endl;
+                    cout<<"STUDENTS MUST REGISTER FIRST IN ORDER TO ACCESS THESE PRIVILEDGES"<<endl;
+                    
+                    
+                    
+                    break;
+                }
+
+            case 2:
+                {
+                    cout<<"HOTLINES NUMBERS"<<endl;
+                    cout<<"----------------"<<endl;
+                    cout<<"COVID-19 HOTLINE          ="<<setw(15)<<"+60133340646"<<endl;
+                    cout<<"UTM AMBULANCE             ="<<setw(15)<<"+6075530999"<<endl;
+                    cout<<"UTM HEALTH CENTRE         ="<<setw(15)<<"+60197756765"<<endl;
+                    cout<<"STUDENT AFFAIRS HOTLINE 1 ="<<setw(15)<<"+6075530279"<<endl;
+                    cout<<"STUDENT AFFAIRS HOTLINE 2 ="<<setw(15)<<"+6075530265"<<endl;
+
+                    break;
+                }
+            
+            case 3:
+                {
+                    cout<<"Protocol for positive cases:"<<endl;
+
+                    cout<<"The isolation order for patients infected with ALL variants are as follows:"<<endl;
+                    cout<<"1. Seven days for individuals who are fully vaccinated and have no symptoms."<<endl;
+                    cout<<"2. 10 days for individuals who have incomplete vaccinations, or are symptomatic cases."<<endl<<endl;
+
+                    cout<<"A health assessment will be made before discharge can be granted."<<endl<<endl;
+
+                    cout<<"Protocol for close contacts:"<<endl;
+
+                    cout<<"Isolation orders for close contacts of positive cases, including Omicron cases, are as follows:"<<endl;
+
+                    cout<<"1. Five days for individuals who have already received the booster dose."<<endl;
+                    cout<<"2. Seven days for individuals who are fully vaccinated, and those who are partially or are not vaccinated."<<endl;
+                    cout<<"3. If symptomatic, take the RTK-Ag test, either on your own or at a nearby health facility for further treatment."<<endl:
+                    cout<<"4. Results of the self-test must be submitted in the Mysejahtera application. "<<endl;
+                    cout<<"5. Perform the RTK-Ag test, either at a government or private facility, on the fifth day and the seventh day (for those undergoing seven days of isolation)."<<endl;
+                    cout<<"6. If the test results are negative and the individual is asymptomatic, a quarantine release will be given on the same day the test is performed."<<endl;
+                    break;
+                }
+
+            case 4:
+                cout<< "Returning to AppMenu..."<<endl;
+                userAppMenu();
+                break;     
+        
+            default:
+                cout<<"Please enter the valid number(1-4)"<<endl;
+                break;
+        }
+    }
+};
+
 //function prototype
+void getUserData(User []);
+void LoginMenu(User [], admin , hotspotInfo []);
 loginReturnData userLoginMenu(User []);
-bool adminLoginMenu(admin);
+void userRegister(User user[]);
 void userAppMenu(loginReturnData, User[], hotspotInfo[]);
+bool adminLoginMenu(admin);
 void adminAppMenu(hotspotInfo[]);
 void menuPrint(int);
 
@@ -273,6 +359,93 @@ loginReturnData userLoginMenu(User user[]){
     return logReturn;
 }
 
+//----------------------------------------userRegister function----------------------------------------//
+void userRegister(User user[]){
+    string temp1[6];
+    int userNumTemp = USER_NUM;
+    ofstream outputFile("User_Data.txt");
+    
+    outputFile <<  USER_NUM + 1;
+
+    for (int i = 0; i < USER_NUM*5; i++)
+    {
+        outputFile << ;
+    }
+
+    cout << "Enter username     : ";
+    getline(cin, temp1[0]);
+    cout << "Enter password     : ";
+    getline(cin, temp1[1]);
+    cout << "Enter full name    : ";
+    getline(cin, temp1[2]);
+    cout << "Enter address      : ";
+    getline(cin, temp1[3]);
+    cout << "Enter vaccine type : ";
+    getline(cin, temp1[4]);
+    cout << "Enter phone number : ";
+    getline(cin, temp1[5]);
+
+    for (int i = 0; i < 5; i++)
+    {
+        outputFile << temp1[i] << endl;
+    }
+    
+    outputFile.close();
+}
+
+//-----------------------------------------userAppMenu function----------------------------------------//
+void userAppMenu(loginReturnData logData, User user[], hotspotInfo hotspotArray[]){
+
+    int choice;
+
+    while (true)
+    {
+        menuPrint(2); //print userAppMenu screen
+
+        cin >> choice;
+
+        switch(choice){
+        case 1:
+            break;
+        case 2:
+        {
+            user[logData.userIndex].userHealth.setHealthCondition();
+
+            if (user[logData.userIndex].userHealth.getTestResult() == "positive")
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if(user[logData.userIndex].userHealth.getLocation().compare(hotspotArray[i].getHotspotArea()) == 0)
+                    {
+                        hotspotArray[i] + 1;
+                    }
+                }
+            }
+
+            break;
+        }
+            
+        case 3:
+            //HotspotInfo();
+            break;
+
+        case 4:
+            //GuideInfo();
+            break;
+            
+        case 5:
+            cout<< "Exiting now..."<<endl;
+            goto exit_loop;
+        
+        default:
+            cout<<"Please enter the valid number(1-6)"<<endl;
+            break;
+        }
+    }
+
+    exit_loop: ;
+}
+
 //---------------------------------------adminLoginMenu function---------------------------------------//
 bool adminLoginMenu(admin admin_){
 
@@ -347,58 +520,6 @@ void adminAppMenu(hotspotInfo hotspotArray[]){
 
 }
 
-//--------------------------------------------menu function--------------------------------------------//
-void userAppMenu(loginReturnData logData, User user[], hotspotInfo hotspotArray[]){
-    int choice;
-
-    while (true)
-    {
-        menuPrint(2); //print userAppMenu screen
-
-        cin >> choice;
-
-        switch(choice){
-        case 1:
-            break;
-        case 2:
-        {
-            user[logData.userIndex].userHealth.setHealthCondition();
-
-            if (user[logData.userIndex].userHealth.getTestResult() == "positive")
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    if(user[logData.userIndex].userHealth.getLocation().compare(hotspotArray[i].getHotspotArea()) == 0)
-                    {
-                        hotspotArray[i] + 1;
-                    }
-                }
-            }
-
-            break;
-        }
-            
-        case 3:
-            //HotspotInfo();
-            break;
-
-        case 4:
-            //GuideInfo();
-            break;
-            
-        case 5:
-            cout<< "Exiting now..."<<endl;
-            goto exit_loop;
-        
-        default:
-            cout<<"Please enter the valid number(1-6)"<<endl;
-            break;
-        }
-    }
-
-    exit_loop: ;
-}
-
 //-----------------------------------------menuPrint function------------------------------------------//
 void menuPrint(int n){
     
@@ -441,6 +562,20 @@ void menuPrint(int n){
         cout<< setw(10)<<" 3. "<<setw(10)<<"HotSpot Info"<<endl;
         cout<< setw(10)<<" 4. "<<setw(10)<<"Guide Info"<<endl;
         cout<< setw(10)<<" 5. "<<setw(10)<<"Exit"<<endl;
+    }
+
+    if (n == 3)
+    {
+        cout<<"\n          GUIDE INFO         "<<endl;
+        cout<<"   FOR COV19 ASSIST AND TRACING SYSTEM    "<<endl;
+        cout<<"------------------------------------------"<<endl;
+        cout<<"         PLEASE ENTER YOUR CHOICE         "<<endl;
+        cout<<"------------------------------------------"<<endl;
+        
+        cout<<left<< setw(10)<<" 1. "<<setw(10)<<"Facilities"<<endl;
+        cout<< setw(10)<<" 2. "<<setw(10)<<"Hotline Numbers"<<endl;
+        cout<< setw(10)<<" 3. "<<setw(10)<<"Standard Operation Procedure (SOP)"<<endl;
+        cout<< setw(10)<<" 4. "<<setw(10)<<"Return"<<endl;
     }
 }
 
