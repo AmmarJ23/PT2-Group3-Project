179024c87605655fe904271ad7f5b135c9069c6b
AmmarJ23 <ammar.jmldn@outlook.com>
2022-06-17 21:27:29 +0800

    commit

--- scratch.cpp
+++ scratch.cpp
@@ -1,30 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class returnClass
+{
+    private:
+        string item1, item2;
+        int item3;
+    public:
+        returnClass()
+        {
+            item1 = "This is item 1";
+            item2 = "This is item 2";
+            item3 = 10;
+        }
+
+        void print()
+        {
+            cout << "Item1 = " << item1 << endl;
+            cout << "Item2 = " << item2 << endl;
+            cout << "Item3 = " << item3 << endl;
+        }
+};
+
 class test
 {
     private:
         int testItem;
+        returnClass* returnClassObj;
     public:
         test(int n)
         {
             testItem = n;
         }
 
-        int getItem(){
+        int getItem()
+        {
             return testItem;
+        }
+
+        void setReturnClass(returnClass obj)
+        {
+            returnClassObj = &obj;
+        }
+
+        returnClass* getReturnClass()
+        {
+            return returnClassObj;
         }
 };
 
 int main()
 {
-    test objArray[4] = {test(1), test(2), test(3), test(4)};
+    test objTestArray[4] = {test(1), test(2), test(3), test(4)}; // create array of "test" class
 
     test *ptrObj;
-    ptrObj = &objArray[1];
+    ptrObj = &objTestArray[1];                      // create pointer that points to 2nd element in test class array
 
+    returnClass objReturn[2];                       // create array of returnClass class
 
-    cout << ptrObj->getItem();
+    objTestArray[0].setReturnClass(objReturn[0]);   // the returnClass pointer in test class to 1st element in objReturn array
+
+    objTestArray[0].getReturnClass()->print();      // prints  objReturn[0] using print() method
+
+    cout << ptrObj->getItem() << endl;              // using the pointer to 2nd element in objTestArray, get the item variable and output to console
+
+    system("PAUSE");
 
     return 0;
 }
