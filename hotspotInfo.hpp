75cfb06cac75ed80018760d1187678b8f283ef4d
AmmarJ23 <ammar.jmldn@outlook.com>
2022-06-22 15:36:07 +0800

    Separate hotspotInfo class files

--- hotspotInfo.hpp
+++ hotspotInfo.hpp
@@ -12,42 +12,12 @@ class hotspotInfo{
         string hotspotArea, areaStatus;
         int infectedNum;
     public:
-        hotspotInfo(){}
-
-        hotspotInfo(string ha, int infNum)
-        {
-            hotspotArea = ha;
-            infectedNum = infNum;
-
-            areaStatusCalc();
-        }
-
-        void areaStatusCalc()
-        {
-            if(infectedNum <=3){areaStatus = "Safe";}
-            else {areaStatus = "Not safe";}
-        }
-
-        void print()
-        {   
-            cout << "COVID-19 INFO FOR :" << hotspotArea << endl;
-            cout << "Area Status: " << areaStatus << endl;
-            cout << "Number of Infected People : " << infectedNum << endl << endl;
-
-        }
-
-        hotspotInfo operator+(int i){
-            hotspotInfo obj;
-            obj.hotspotArea = hotspotArea;
-            obj.infectedNum = infectedNum + i;
-
-            if(obj.infectedNum <=3){obj.areaStatus = "Safe";}
-            else {obj.areaStatus = "Not safe";}
-
-            return obj;
-        }
-
-        string getHotspotArea(){return hotspotArea;}
+        hotspotInfo();
+        hotspotInfo(string ha, int infNum);
+        void areaStatusCalc();
+        void print();
+        hotspotInfo operator+(int i);
+        string getHotspotArea();
 };
 
 #endif
